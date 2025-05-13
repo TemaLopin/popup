990ea6448bafbd43dfbfb73e04e7e5e23ed3bf00
Tema Lopin <a.lopin@auchan.ru>
2025-05-13 17:52:06 +0300

    Add regExp for date

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -1,12 +1,13 @@
 import './style.css'
 
-export const Input = ({ type, onChange, ...props }) => {
-  // const [value, setValue] = useState('')
+const regExpNumber = /^[0-9.]+$/
 
+export const Input = ({ type, onChange, ...props }) => {
   const onChangeInput = ({ target: { value } }) => {
-    
     if (type !== 'date') return onChange(value)
+
     if (value.length > 10) return
+    if (!regExpNumber.test(value)) return
     if (value.length === 2) return onChange(`${value}.`)
     if (value.length === 5) return onChange(`${value}.`)
     return onChange(value)
